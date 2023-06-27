5f88066fac3a8f5e55f21fc7365ffe231e79fd87
Betelhem-hailu <betelhemhailu811@gmail.com>
2023-06-28 01:43:23 +0300

    design_modified

--- Frontend-Dev/ideahive_webapp/src/components/Edit.js
+++ Frontend-Dev/ideahive_webapp/src/components/Edit.js
@@ -1,92 +1,233 @@
 // Importing necessary libraries popup and MUI/MUI-icons
 import { useContext, useState, useEffect } from "react";
 import { PostContext } from "../context/PostState";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import {
   Button,
   InputLabel,
   Select,
   MenuItem,
-  TextareaAutosize,
   Stack,
   TextField,
+  FormControl,
+  Box,
 } from "@mui/material";
-import "reactjs-popup/dist/index.css";
+import EditIcon from '@mui/icons-material/Edit';
 
-
-const Edit = (props) => {
-  const { posts, editPost} = useContext(PostContext);  
+const Edit = () => {
+  const { posts, editPost } = useContext(PostContext);
   const [selectedpost, setSelectedPost] = useState({
-    id:'',
-    userId: '',
+    id: "",
+    userId: "",
     title: "",
     description: "",
     category: "",
     // file: null,
   });
 
-  const currentPostId = props.match.params.id;
-  console.log(props.match.params.id);
+  const currentPostId = useParams();
   const navigate = useNavigate();
 
-  useEffect(()=>{
-    const postId = currentPostId;
-    selectedpost = posts.find(post => post.id === postId)
-    setSelectedPost(selectedpost);
-  },[currentPostId, posts])
+  useEffect(() => {
+    const postId = currentPostId.postid;
+    setSelectedPost(posts.find((post) => post.id === Number(postId)));
+  }, [currentPostId, posts]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setSelectedPost({...selectedpost, [name]: value});
+    setSelectedPost({ ...selectedpost, [name]: value });
   };
+
 
   const handleSubmit = () => {
     editPost(selectedpost);
-    navigate('/post');
+    navigate("/post");
   };
+
+  // <Stack direction="column" mt={"50px"} spacing={2} width={"50vw"} justifyContent="center">
+  //   <form onSubmit={handleSubmit}>
+  //   <InputLabel>Title</InputLabel>
+  //     <TextField type="text" name="title" value={selectedpost.title} onChange={handleChange}/>
+  //     <InputLabel>Description</InputLabel>
+  //     <TextareaAutosize
+  //       minRows={5}
+  //       name="body"
+  //       value={selectedpost.body}
+  //       onChange={handleChange}
+  //     ></TextareaAutosize>
+  //     <InputLabel>Category</InputLabel>
+  //     <Select label="category" name="category" value={selectedpost.category} onChange={handleChange}>
+  //     <MenuItem value={"agriculture"}>Agriculture</MenuItem>
+  //       <MenuItem value={"education"}>Education</MenuItem>
+  //       <MenuItem value={"healthcare"}>Healthcare</MenuItem>
+  //       <MenuItem value={"industry"}>Industry</MenuItem>
+  //       <MenuItem value={"information technology"}>Infromation Technology</MenuItem>
+  //     </Select>
+  //    <Stack direction='row'>
+  //    <Button
+  //    variant="contained"
+  //    sx={{ m: 4, p: 1.5 }}
+  //    color="primary"
+  //    type="submit"
+  //  >
+  //    Update
+  //  </Button>
+  //  <Button
+  //  variant="contained"
+  //  sx={{ m: 4, p: 1.5 }}
+  //  color="error"
+  //  type="submit"
+  // >
+  //  Cancel
+  // </Button>
+  // </Stack>
+  // </form>
+
+
+  // <Box
+  //           position="absolute"
+  //           top={344}
+  //           left={397}
+  //           bottom={101}
+  //           right={397}
+  //           bgcolor="rgba(255, 255, 255, 0.7)"
+  //           display="flex"
+  //       justifyContent="center"
+  //       alignItems="center"
+  //       sx={{
+  //         borderRadius: "10px",
+  //         border: "1px solid black",
+  //       }}
+  //         >
+  //         <Button
+  //         variant="contained"
+  //         sx={{
+  //           width: "100px",
+  //           background: "#60dfe69e",
+  //           color: "#000000",
+  //           fontSize: "18",
+  //           borderRadius: "10px",
+  //           p: "0.2rem",
+  //           mt: "-100px",
+  //           ":hover": {
+  //             background: "#60dfe69e",
+  //             color: "#FFFFFF",
+  //           },
+  //         }}
+  //       >
+  //         Change
+  //       </Button>
+  //       </Box>
+
   return (
     //edit ui with functionality
-        <Stack ml={"5vw"} mt={"20px"} spacing={2} width={"30vw"}>
-        <form onSubmit={handleSubmit}>
-        <InputLabel>Title</InputLabel>
-          <TextField type="text" name="title" value={selectedpost.title} onChange={handleChange}/>
-          <InputLabel>Description</InputLabel>
-          <TextareaAutosize
-            minRows={5}
-            name="description"
-            value={selectedpost.body}
+    <Box display="flex" justifyContent="center">
+      <Stack
+        mt="30px"
+        p="30px"
+        spacing={3}
+        direction="column"
+        width="40vw"
+        sx={{
+          boxShadow: "0px 5px 15px 3px rgba(0, 0, 0, 0.25)",
+        }}
+      >
+        <TextField
+          label="Title"
+          name="title"
+          value={selectedpost.title}
+          variant="standard"
+          onChange={handleChange}
+        />
+        <TextField
+          label="Description"
+          name="body"
+          value={selectedpost.body}
+          multiline
+          maxRows={5}
+          variant="standard"
+          onChange={handleChange}
+        />
+        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel id="demo-simple-select-standard-label">
+            Category
+          </InputLabel>
+          <Select
+            labelId="demo-simple-select-standard-label"
+            id="demo-simple-select-standard"
+            name="category"
+            value={selectedpost.category}
             onChange={handleChange}
-          ></TextareaAutosize>
-          <InputLabel>Category</InputLabel>
-          <Select label="category" name="category" value={selectedpost.category} onChange={handleChange}>
-          <MenuItem value={"agriculture"}>Agriculture</MenuItem>
-            <MenuItem value={"education"}>Education</MenuItem>
-            <MenuItem value={"healthcare"}>Healthcare</MenuItem>
-            <MenuItem value={"industry"}>Industry</MenuItem>
-            <MenuItem value={"information technology"}>Infromation Technology</MenuItem>
+            label="Category"
+          >
+            <MenuItem value="agriculture">Agriculture</MenuItem>
+            <MenuItem value="education">Education</MenuItem>
+            <MenuItem value="health_care">Health Care</MenuItem>
+            <MenuItem value="industry">Industry</MenuItem>
+            <MenuItem value="information technology">
+              Infromation Technology
+            </MenuItem>
           </Select>
-         <Stack direction='row'>
-         <Button
-         variant="contained"
-         sx={{ m: 4, p: 1.5 }}
-         color="primary"
-         type="submit"
-        //  onClick={()}
-       >
-         Update
-       </Button>
-       <Button
-       variant="contained"
-       sx={{ m: 4, p: 1.5 }}
-       color="error"
-       type="submit"
-      //  onClick={() => close()}
-     >
-       Cancel
-     </Button>
-     </Stack>
-     </form>
-    </Stack>
+        </FormControl>
+        <Stack direction="row" justifyContent= "flex-end"><EditIcon/></Stack>
+        <Box
+        component="img"
+        src={selectedpost.image}
+        alt={selectedpost.title}
+          sx={{
+            width: "430px",
+            height: "150px",
+            borderRadius: "10px",
+            border: "1px solid black",
+          }}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+        </Box>
+        
+        <Stack direction={"row"} spacing={4} justifyContent="center" ml="30px">
+          <Button
+            variant="contained"
+            sx={{
+              width: "150px",
+              background: "#FBDF5F",
+              color: "#000000",
+              fontSize: "24",
+              border: "1px solid black",
+              borderRadius: "10px",
+              p: "0.5rem",
+              ":hover": {
+                background: "#FBDF5F",
+                color: "#FFFFFF",
+              },
+            }}
+            onClick={handleSubmit}
+          >
+            Update
+          </Button>
+          <Button
+            variant="contained"
+            sx={{
+              width: "150px",
+              background: "#FFFFFF",
+              color: "#000000",
+              fontSize: "24",
+              border: "1px solid black",
+              borderRadius: "10px",
+              p: "0.5rem",
+              ":hover": {
+                background: "#FBDF5F",
+                color: "#FFFFFF",
+              },
+            }}
+            onClick={() => navigate("/post")}
+          >
+            Cancel
+          </Button>
+        </Stack>
+      </Stack>
+    </Box>
   );
 };
 
--- Frontend-Dev/ideahive_webapp/src/components/UserPage.js
+++ Frontend-Dev/ideahive_webapp/src/components/UserPage.js
@@ -1,25 +1,98 @@
 // Importing necessary libraries for routing and MUI/MUI-icons
 import { useContext } from "react";
-import { useParams } from "react-router-dom";
-import { Box, Stack} from "@mui/material";
+import { Link, useParams } from "react-router-dom";
+import { Box, Stack, Button, Grid, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 import { PostContext } from "../context/PostState";
 import ProfileSection from "./ProfileSection";
 import UserPost from "./UserPost";
 
 const UserPage = () => {
-  const { posts} = useContext(PostContext);
+  const { posts } = useContext(PostContext);
+  // const {accounts} = useContext(AccountContext)
   const { userid } = useParams();
-console.log(posts);
-  const userposts = [...new Set(posts.filter((q) => q.userId == userid))];
+  
+  const userposts = [...new Set(posts.filter((q) => q.userId === Number(userid)))];
+  // const useraccount = [...new Set(accounts.filter((q)=> q.userId == userid))]
+  
+
+
   return (
     //users page which can see profile and post
-    <Box sx={{ p: 8, bgColor: "#F5F5F5" }}>
-    <ProfileSection />
-    {userposts.map((post) => (
-      <Stack mt='20px' >
-    <UserPost userid ={userid} post={post}/>
-    </Stack>
-    ))}
+    <Box sx={{ p: 8, bgColor: "#FFFFFF" }}>
+      <ProfileSection userid={userid} />
+      <Stack direction= "row" justifyContent="flex-end">
+      <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+        <Select
+          labelId="demo-simple-select-standard-label"
+          id="demo-simple-select-standard"
+          value="ALL"
+        >
+          <MenuItem value="ALL">
+          ALL
+          </MenuItem>
+          <MenuItem value="">Agriculture</MenuItem>
+          <MenuItem value="">Eduaction</MenuItem>
+          <MenuItem value="">Health Care</MenuItem>
+          <MenuItem value="">Industry</MenuItem>
+          <MenuItem value="">Infromation Technology</MenuItem>
+        </Select>
+        </FormControl>
+      </Stack>
+      <Stack direction="row" spacing={8} mt="50px">
+        <Stack
+          direction="column"
+          width="350px"
+          height="387px"
+          p="50px"
+          spacing={5}
+        >
+          <Button variant="text" sx={{
+            width: "300px",
+            color: "#000000",
+            fontSize:"24",
+            borderBottom: '1px solid black',
+            borderRadius: '0px',
+            justifyContent: 'flex-start',
+            ":hover": {
+              background: "#FBDF5F",
+              color: "#000000",
+              borderBottom: '0px',
+              boxShadow: '0px 5px 15px 3px rgba(0, 0, 0, 0.25)'
+            },
+          }} >Edit Profile</Button>
+          <Link to='/createpost'>
+          <Button variant="text" sx={{
+            width: "300px",
+            color: "#000000",
+            fontSize:"24",
+            borderBottom: '1px solid black',
+            borderRadius: '0px',
+            justifyContent: 'flex-start',
+            ":hover": {
+              background: "#FBDF5F",
+              color: "#000000",
+              borderBottom: '0px',
+              boxShadow: '0px 5px 15px 3px rgba(0, 0, 0, 0.25)'
+            },
+          }}>Create Post</Button>
+          </Link>
+        </Stack>
+        <Box>
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          >
+            {userposts.map((post) => (
+              <Grid item xs={6} mt="20px">
+                <Stack position="relative">
+                  <UserPost userid={userid} post={post} />
+                </Stack>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      </Stack>
     </Box>
   );
 };
